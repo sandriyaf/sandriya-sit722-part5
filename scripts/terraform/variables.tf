33ff94cefbc7275505b7f49a70e7d792a5b382d4
Sandriya <sandriyafernandes10@gmail.com>
2024-09-25 16:31:17 +1000

    update

---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,14 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "sandriyapart5"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version {   
+  default = "1.29.7" 
+}
+
