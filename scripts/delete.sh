c1d04a88ef917b9b4d76cf8b54a5c9906b2be969
Sandriya <sandriyafernandes10@gmail.com>
2024-09-25 18:35:16 +1000

    update

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,4 @@
 # Your Solution
+#!/bin/bash
+kubectl delete -f kubernetes/book-catalog-deployment.yaml
+kubectl delete -f kubernetes/inventory-management-deployment.yaml
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,4 @@
 # Your Solution
+#!/bin/bash
+kubectl apply -f kubernetes/book-catalog-deployment.yaml
+kubectl apply -f kubernetes/inventory-management-deployment.yaml
