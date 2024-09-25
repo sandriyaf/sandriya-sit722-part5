a1823aa70b368a2a04960c0203b164095c255fbb
Sandriya <sandriyafernandes10@gmail.com>
2024-09-25 18:56:04 +1000

    update

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -15,7 +15,7 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-docker compose -f ./docker-compose.yml up --build
+docker compose -f ./docker-compose.yml build
 
 docker tag books $CONTAINER_REGISTRY/books:$VERSION 
 
